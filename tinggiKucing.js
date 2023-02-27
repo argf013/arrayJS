965cff54d9bab5d927d5a8c5c3568bb04ac1f5ff
Muhamad Arfa <20240075@polimedia.ac.id>
2023-02-27 14:04:53 +0700

    Create tinggiKucing.js

---
+++ tinggiKucing.js
@@ -0,0 +1,19 @@
+function cari_kucing_pendek(n, p, tinggi_kucing) {
+  // Mengurutkan tinggi semua kucing secara menurun
+  tinggi_kucing.sort((a, b) => b - a);
+
+  // Mengambil kucing dengan urutan ke-p dari urutan terpendek
+  const kucing_pendek = tinggi_kucing[p-1];
+
+  return kucing_pendek;
+}
+
+const n = 5;
+const p = 2;
+const tinggi_kucing = [5, 4, 3, 2, 1];
+
+const kucing_pendek = cari_kucing_pendek(n, p, tinggi_kucing);
+console.log(kucing_pendek);
+
+
+exports.cari_kucing_pendek = cari_kucing_pendek;

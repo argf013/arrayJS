3276878d643b053d17c293338bde599f446f8691
Muhamad Arfa <20240075@polimedia.ac.id>
2023-02-27 13:44:13 +0700

    Create prima.js

---
+++ prima.js
@@ -0,0 +1,45 @@
+// Membaca masukan bilangan
+const bilangan = parseInt(prompt("Masukkan bilangan:"));
+
+// Fungsi untuk memeriksa apakah suatu bilangan prima atau tidak
+function isPrima(n) {
+  if (n <= 1) {
+    return false;
+  }
+
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+// Fungsi untuk memeriksa apakah suatu bilangan agak prima atau tidak
+function isAgakPrima(n) {
+  // Periksa apakah bilangan itu prima
+  if (isPrima(n)) {
+    return true;
+  }
+
+  // Cari dua faktor positif lainnya yang mungkin
+  let count = 0;
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0 && isPrima(i)) {
+      count++;
+      if (count > 1) {
+        return false;
+      }
+    }
+  }
+
+  return count === 1;
+}
+
+// Cek apakah bilangan tersebut agak prima atau bukan
+if (isAgakPrima(bilangan)) {
+  console.log("YA");
+} else {
+  console.log("BUKAN");
+}

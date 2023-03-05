4fade7a8ec12d695cb313d9eeae542faafaf345f
Muhamad Arfa <20240075@polimedia.ac.id>
2023-03-06 01:48:57 +0700

    Update findPair.js

--- random/findPair.js
+++ random/findPair.js
@@ -22,3 +22,13 @@ function findPair(arr, target) {
 
   return pair;
 }
+
+// Fungsi findPair menerima dua parameter yaitu arr yang merupakan array angka dan target yang merupakan target jumlah dari pasangan angka yang ingin dicari.
+// Variabel pair digunakan untuk menyimpan pasangan angka yang ditemukan.
+// Dilakukan iterasi menggunakan dua buah for loop untuk mencari pasangan angka
+// yang jumlahnya sama dengan target. Pertama, loop luar digunakan untuk memilih angka pertama 
+// dari pasangan dan loop dalam digunakan untuk memilih angka kedua dari pasangan.
+// Variabel j dimulai dari i + 1 agar angka yang dipilih tidak sama dengan angka yang telah dipilih sebelumnya.
+// Jika pasangan angka ditemukan, maka kedua angka tersebut dimasukkan ke dalam array pair dan dilakukan break dari loop.
+// Jika array pair tidak kosong, maka dilakukan break dari loop luar.
+// Fungsi findPair mengembalikan array pair yang berisi pasangan angka yang jumlahnya sama dengan target. Jika tidak ada pasangan angka yang ditemukan, maka array pair akan kosong.
